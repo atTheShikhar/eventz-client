cb0eec7e501eab1496a57029bee57145a558e370
Shikhar Singh <shikharsingh.original@gmail.com>
2021-04-10 23:48:57 +0530

    INIT: create events form initialized

---
+++ src/Pages/Create/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function index(props) {
+    const nextStep = () => {
+
+    };
+    const prevStep = () => {
+        
+    }
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default index
